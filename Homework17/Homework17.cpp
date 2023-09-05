16c599d1eacaa7f74cabd183e5eacb42558a1bc0
user <user@WIN-4HU5T7LM44J>
2023-09-05 19:04:14 +0300

    Добавьте файлы проекта.

---
+++ Homework17/Homework17.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+using namespace std;
+//1 
+// 
+//int f(int num, int x);
+//
+//int main()
+//{
+//    int n, x;
+//
+//    cout << "Введите число: ";
+//    cin >> n;
+//    cout << "Введите степень: ";
+//    cin >> x;
+//    cout << n << " в степени " << x << " = " << f(n, x);
+//}
+//
+//int f(int n, int x)
+//{
+//    int t = 1;
+//
+//    for (int i = 0; i < x; i++)
+//    {
+//        t *= n;
+//    }
+//
+//    return t;
+//}
+
+//2 
+ 
+int f(int num, int x);
+
+int main()
+{
+    int n, x;
+
+    cout << "Введите 1 число: ";
+    cin >> n;
+    cout << "Введите 2 число: ";
+    cin >> x;
+    cout << "Сумма чисел между " << n << " и " << x << " = " << f(n, x);
+}
+
+int f(int n, int x)
+{
+    int t = 0;
+
+    while (n < x) 
+    {
+        t = t + n;
+        n++;
+    }
+
+    return t;
+}
+
+
+
+
